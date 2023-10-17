eaa504948799166b26d2b9b519ece8c715095cd3
zedd-08 <manish.96@live.com>
2023-10-17 19:22:51 +0200

    Updated torch install command

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,7 @@
 echo "Creating venv for llama2 and GPTQ"
 conda create -n llama2 python=3.10
 conda activate llama2
-conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117
 
 pip install 'transformers>=4.32.0' 'optimum>=1.12.0' cchardet 'ctransformers[cuda]>=0.2.24'
 pip install auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu117/
